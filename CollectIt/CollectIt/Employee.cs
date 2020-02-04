c3deee4e3e71cea17ca7caf83fdd7f7a00da090a
Guilherme Marques <guilherme.marques@quartalfs.com>
2020-02-04 12:48:11 +0000

    Include Project

---
+++ CollectIt/CollectIt/Employee.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace CollectIt
+{
+
+    public class EmployeeComparer : IEqualityComparer<Employee>
+    {
+        public bool Equals(Employee x, Employee y)
+        {
+            return x.Name == y.Name;
+        }
+
+        public int GetHashCode(Employee obj)
+        {
+            return obj.Name.GetHashCode();
+        }
+    }
+
+
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int DepartmentId { get; set; }
+    }    
+}
---
+++ DataStructures/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using DataStructures.Basic;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var buffer = new CircularBuffer();
+
+            ProcessInput(buffer);       
+            ProcessBuffer(buffer);
+        }
+
+        private static void ProcessBuffer(CircularBuffer buffer)
+        {
+            var sum = 0.0;
+            Console.WriteLine("Buffer: ");
+            while (!buffer.IsEmpty)
+            {
+                sum += buffer.Read();
+            }
+            Console.WriteLine(sum);
+        }
+
+        private static void ProcessInput(CircularBuffer buffer)
+        {
+            while (true)
+            {
+                var value = 0.0;
+                var input = Console.ReadLine();
+
+                if (double.TryParse(input, out value))
+                {
+                    buffer.Write(value);
+                    continue;
+                }
+                break;
+            }
+        }
+    }
+}
